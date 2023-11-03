a37567739c34799bea5bb8e569afd2dbd00203fe
sylvain-galoustoff <sylvain.galoustoff@myexpressdriver.com>
2023-11-03 16:22:11 +0100

    Fix avatar images strech

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -4,12 +4,19 @@ function photographerTemplate(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+
+        const imgWrapper = document.createElement('div')
+        imgWrapper.classList.add('rounded-avatar')
+
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+
+        imgWrapper.appendChild(img)
+
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
+        article.appendChild(imgWrapper);
         article.appendChild(h2);
         return (article);
     }
