246e9b12efa90001762f7a2471fcdd11304e8e42
sylvain-galoustoff <sylvain.galoustoff@myexpressdriver.com>
2023-11-24 10:19:44 +0100

    Lightbox : reset image insertion

--- scripts/ui/lightbox.js
+++ scripts/ui/lightbox.js
@@ -1,48 +1,24 @@
 import Slide from "../templates/Slide.js";
 
 export default function lightbox() {
+  const mediaCard = document.querySelectorAll(".media-card a");
 
-    const mediaCard = document.querySelectorAll('.media-card')
-    const medias = document.querySelectorAll('.media-card-thumbnail a > *')
-    console.log(medias);
+  const lightbox = document.getElementById("lightbox");
+  const closeButton = document.getElementById("close-lightbox");
 
-    const lightbox = document.getElementById('lightbox')
-    const lightboxContent = document.getElementById('lightbox-content')
-    const closeButton = document.getElementById('close-lightbox')
-    const lightboxArrow = document.querySelectorAll('.lightbox-arrow')
+  /* ECOUTEURS */
+  mediaCard.forEach((card) => {
+    card.addEventListener("click", openLightbox);
+  });
 
-    /* ECOUTEURS */
-    mediaCard.forEach((card, index) => {
-        card.addEventListener('click', openLightbox.bind(this, index))
-    })
+  closeButton.addEventListener("click", closeLightbox);
 
-    closeButton.addEventListener('click', closeLightbox)
-    lightboxArrow.forEach(arrow => {
-        const direction = arrow.getAttribute('id')
-        arrow.addEventListener('click', changeSlide.bind(this, direction))
-    })
+  /* Open and use lightbox */
+  function openLightbox() {
+    lightbox.style.display = "flex";
+  }
 
-    /* FONCTIONS */
-    function openLightbox(index) {
-        lightbox.style.display = 'flex'
-        medias.forEach(media => {
-            const cloneMedia = media.cloneNode(true)
-            const slide = Slide(cloneMedia)
-            lightboxContent.appendChild(slide)
-        })
-        console.log(index);
-        lightboxContent.style.transform = 'translateX(-' + index * 100 + '%)'
-    }
-
-    function closeLightbox() {
-        lightbox.style.display = 'none'
-    }
-
-    //Changement de media
-    function changeSlide(direction) {
-
-        console.log(direction);
-    }
-
+  function closeLightbox() {
+    lightbox.style.display = "none";
+  }
 }
-
